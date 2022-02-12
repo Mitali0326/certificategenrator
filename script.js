4c31829947b1e945de3a47d190039368ae74f274
Aarchi Jain <aarchi.jain0409@gmail.com>
2022-02-12 20:38:57 +0530

    style.css updated & canvas library added

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const nameInput = document.getElementById('name')
+const contestInput = document.getElementById('contest')
+const positionInput = document.getElementById('position')
+const dateInput = document.getElementById('date')
+const downloadBtn = document.getElementById('download-btn')
+
+const image = new Image()
+image.src = 'certificate.jpg'
+image.onload = function (){
+	drawImage()
+}
+
+function drawImage() {
+	ctx.drawImage(image, 20, 0, canvas.width, canvas.height)
+	ctx.font = '20px monotype corsiva'
+	ctx.fillStyle = 'black'
+
+	ctx.fillText(nameInput.value, 245, 260)
+
+    ctx.fillText(contestInput.value, 305, 310)
+
+	ctx.fillText(positionInput.value, 470, 285)
+
+    ctx.fillText(dateInput.value, 265, 395)
+
+}
+
+nameInput.addEventListener('input', function () {
+	drawImage()
+})
+
+contestInput.addEventListener('input', function () {
+	drawImage()
+})
+
+positionInput.addEventListener('input', function () {
+	drawImage()
+})
+
+dateInput.addEventListener('input', function () {
+	drawImage()
+})
+
+downloadBtn.addEventListener('click', function () {
+	downloadBtn.href = canvas.toDataURL('image/jpg')
+	downloadBtn.href = canvas.toDataURL('image/pdf')
+	downloadBtn.download = 'Certificate - ' + nameInput.value
+})
+
 window.onload = function () {
     document.getElementById("download")
         .addEventListener("click", () => {
