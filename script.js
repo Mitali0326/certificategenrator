1aa9a68e2e7dab7a973b9747ad2d31cddb45a179
satyamsharma17 <satyamsharma1725@gmail.com>
2022-02-16 16:14:11 +0530

    upload image feature added

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ image.onload = function () {
     drawImage()
 }
 
+document.querySelector("input").oninput = async (evt) => {
+    try {
+      const file = evt.target.files[0];
+      const bitmap = await createImageBitmap(file);
+      const canvas = document.querySelector("canvas");
+      canvas.width = bitmap.width;
+      canvas.height = bitmap.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(bitmap, 0, 0);
+    }
+    catch(err) {
+      console.error(err);
+    }
+  };
+
 function drawImage() {
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
     ctx.font = '35px monotype corsiva'
