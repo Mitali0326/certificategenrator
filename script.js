759f61a4dc2aef3339a36725833298585175512a
Aarchi Jain <aarchi.jain0409@gmail.com>
2022-02-17 02:48:30 +0530

    multi elements added

--- script.js
+++ script.js
@@ -87,28 +87,28 @@ function increase() {
 
 
     //   DRAG AND DROP 1
-    function drag_start(event) {
-        var style = window.getComputedStyle(event.target, null);
-        event.dataTransfer.setData("text",
-        (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-    } 
-    function drag_over(event) { 
-        event.preventDefault(); 
-        return false; 
-    } 
-    function drop(event) { 
-        var offset = event.dataTransfer.getData("text").split(','); 
-        var dm = document.getElementById('dragme');
-        dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-        dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-        ctx.font = "50px monotype corsiva"
-        event.preventDefault();
-        return false;
-    } 
-    var dm = document.getElementById('dragme'); 
-    dm.addEventListener('dragstart',drag_start,false); 
-    document.body.addEventListener('dragover',drag_over,false); 
-    document.body.addEventListener('drop',drop,false); 
+    // function drag_start(event) {
+    //     var style = window.getComputedStyle(event.target, null);
+    //     event.dataTransfer.setData("text",
+    //     (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+    // } 
+    // function drag_over(event) { 
+    //     event.preventDefault(); 
+    //     return false; 
+    // } 
+    // function drop(event) { 
+    //     var offset = event.dataTransfer.getData("text").split(','); 
+    //     var dm = document.getElementById('dragme');
+    //     dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+    //     dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+    //     ctx.font = "50px monotype corsiva"
+    //     event.preventDefault();
+    //     return false;
+    // } 
+    // var dm = document.getElementById('dragme'); 
+    // dm.addEventListener('dragstart',drag_start,false); 
+    // document.body.addEventListener('dragover',drag_over,false); 
+    // document.body.addEventListener('drop',drop,false); 
 
 
    
@@ -125,3 +125,44 @@ function increase() {
 
 // DRAG AND DROP 3
  // end $(function(){});
+
+//  function allowDrop(ev) {
+//     ev.preventDefault();
+//   }
+  
+//   function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+//   }
+  
+//   function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.appendChild(document.getElementById(data));
+//   }
+
+
+function drag_start(event) {
+	var style = window.getComputedStyle(event.target, null);
+	event.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY) + ',' + event.target.getAttribute('data-item'));
+}
+
+function drag_over(event) {
+	event.preventDefault();
+	return false;
+}
+
+function drop(event) {
+	var offset = event.dataTransfer.getData("text/plain").split(',');
+	var dm = document.getElementsByClassName('dragme');
+	dm[parseInt(offset[2])].style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+	dm[parseInt(offset[2])].style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+	event.preventDefault();
+	return false;
+}
+
+var dm = document.getElementsByClassName('dragme');
+for (var i = 0; i < dm.length; i++) {
+	dm[i].addEventListener('dragstart', drag_start, false);
+	document.body.addEventListener('dragover', drag_over, false);
+	document.body.addEventListener('drop', drop, false);
+}
